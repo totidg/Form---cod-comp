e379c7a78c16123af8a81938ee658ae948cc4819
totidg <113801644+totidg@users.noreply.github.com>
2023-03-29 17:45:01 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Form---cod-comp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-jk1nrm)
---
+++ script.js
@@ -0,0 +1,50 @@
+function createRow(event) {
+ 
+  const form = document.querySelector('form');
+  
+  event.preventDefault(); // evita que la página se recargue al enviar el formulario
+  
+  // Asignar a las variables el valor ingresado en los inputs
+  let nombre = document.getElementById('nombre').value;
+  let apellido = document.getElementById('apellido').value;
+  let edad = document.getElementById('edad').value;
+  let dni = document.getElementById('dni').value;
+  let correo = document.getElementById('correo').value;
+
+  // Crea una fila dentro de tbody
+
+  let tableRow = document.createElement('tr');
+
+  // Crea las 5 columnas con los datos
+
+  let tDataNombre = document.createElement('td');
+  tDataNombre.innerHTML = nombre;
+  tableRow.appendChild(tDataNombre);
+
+  let tDataApellido = document.createElement('td');
+  tDataApellido.innerHTML = apellido;
+  tableRow.appendChild(tDataApellido);
+
+  let tDataEdad = document.createElement('td');
+  tDataEdad.innerHTML = edad;
+  tableRow.appendChild(tDataEdad);
+
+  let tDataDni = document.createElement('td');
+  tDataDni.innerHTML = dni;
+  tableRow.appendChild(tDataDni);
+
+  let tDataCorreo = document.createElement('td');
+  tDataCorreo.innerHTML = correo;
+  tableRow.appendChild(tDataCorreo);
+
+  // se le agrega una nueva tabla.
+
+  document
+    .getElementById('tabla')
+    .getElementsByTagName('tbody')[0]
+    .appendChild(tableRow);
+
+  // cada vez que se agregue un usuario, se limpian los datos.
+
+ form.reset();
+}
